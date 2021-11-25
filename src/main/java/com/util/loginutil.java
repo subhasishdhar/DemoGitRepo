69e45652128000b62409e9ecb4131b73aad75864
Subhasish Dhar <subhasishdhar@in.ibm.com>
2021-11-25 18:50:12 +0530

    first git commit

---
+++ src/main/java/com/util/loginutil.java
@@ -0,0 +1,8 @@
+package com.util;
+
+public class loginutil {
+	
+	public void login() {
+		System.out.println("test");
+	}
+}
---
+++ src/test/java/com/test/login.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class login {
+	@Test
+	public void loin() {
+		System.out.println("test");
+	}
+
+}
